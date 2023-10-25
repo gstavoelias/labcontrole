7fe97c097744fead0f9d0af9c91a3e1b8467f9e2
Gustavo Elias Cândido <86082917+gstavoelias@users.noreply.github.com>
2023-10-25 19:31:17 -0300

    Update controle.ino

--- arduino/controle.ino
+++ arduino/controle.ino
@@ -1,61 +1,65 @@
-# include < PETEletrica .h >
+include < PETEletrica .h >
 
-// funcao do filtro de kalman
- double KALMAN ( double U){
- static const double R = 40; // covariancia do ruido
- static const double H = 1.00; // mapa de medicao escalar
- static double Q = 10; // covariancia estimada inicial
- static double P = 0; // erro covariancia inicial
- static double U_hat = 15; // estado estimado inicial
- static double K = 0; // ganho de kalman inicial
+//kalman filtro
+double KALMAN(double U){
+  static const double R = 40;//noise covariance
+  static const double H = 1.00;//measurement map scalar
+  static double Q = 10;//initial estimated covariance
+  static double P = 0;//initial error covariance
+  static double U_hat = 15;//initial estimated state
+  static double K = 0;//initial kalman gain
 
- K = P*H /( H*P*H+R);
- U_hat = U_hat + K *(U -H* U_hat );
+  K = P*H/(H*P*H+R);
+  U_hat = U_hat + K*(U-H*U_hat);
 
- P = (1 - K*H)*P+Q;
+  P = (1-K*H)*P+Q;
 
- return U_hat ;
+  return U_hat;
+}
+
+double filtrado;
+
+int trigger = 3, echo = 2;
+int IN1 = 4, IN2 = 5, IN3 = 6, IN4 = 7;
+float d = 0;
+SensorUltrassonico ultrassonico(trigger, echo);
+Motores motor(IN1, IN2, IN3, IN4);
+
+void setup() {
+ Serial.begin(9600); 
  }
 
- double filtrado ;
- int trigger = 3, echo = 2;
- int IN1 = 4, IN2 = 5, IN3 = 6, IN4 = 7;
- float d = 0;
- SensorUltrassonico ultrassonico ( trigger , echo );
- Motores motor ( IN1 , IN2 , IN3 , IN4 );
+void loop() {
 
- void setup () {
- Serial . begin (9600) ;
- }
+  long microsec = ultrassonico.timing();
+  d = ultrassonico.convert(microsec, SensorUltrassonico::CM);
+  delay(10);
+  
+  Serial.print("Distância: ");//usando só de comparação
+  Serial.print(d);//usando só de comparação
+  Serial.print(" / ");
+  filtrado = KALMAN(d);//chama a função do filtro
+  Serial.print("Filtrados: ");//indica a saída filtrada
+  Serial.println(filtrado);//printa a saída filtrada
 
- void loop () {
+  delay(250); 
+  motor.set_motors(0, 100);
 
- long microsec = ultrassonico . timing () ;
- d = ultrassonico . convert ( microsec , SensorUltrassonico :: CM );
- delay (10) ;
+  
 
- Serial . print (" Distancia : ");// usando so de comparacao
- Serial . print (d);// usando so de comparacao
- Serial . print (" / ");
- filtrado = KALMAN (d);// chama a funcao do filtro
- Serial . print (" Filtrados : ");// indica a saida filtrada
- Serial . println ( filtrado );// printa a saida filtrada
+while (d < 5 ||  d > 18) {
+  long microsec = ultrassonico.timing();
+  d = ultrassonico.convert(microsec, SensorUltrassonico::CM);
+  delay(10);
 
- delay (250) ;
- motor . set_motors (0 , 1000) ;
+  Serial.print("Distância: ");//usando só de comparação
+  Serial.print(d);//usando só de comparação
+  Serial.print(" / ");
+  filtrado = KALMAN(d);//chama a função do filtro
+  Serial.print("Filtrados: ");//indica a saída filtrada
+  Serial.println(filtrado);//printa a saída filtrada
 
- while (d < 5 || d > 18) {
- long microsec = ultrassonico . timing () ;
- d = ultrassonico . convert ( microsec , SensorUltrassonico :: CM );
- delay (10) ;
-
- Serial . print (" Distancia : ");// usando so de comparacao
- Serial . print (d);// usando so de comparacao
- Serial . print (" / ");
- filtrado = KALMAN (d);// chama a funcao do filtro
- Serial . print (" Filtrados : ");// indica a saida filtrada
- Serial . println ( filtrado );// printa a saida filtrada
- delay (250) ;
-motor . set_motors (0 , 0) ;
- }
- }
+  delay(250); 
+  motor.set_motors(0, 0);
+}
+}
